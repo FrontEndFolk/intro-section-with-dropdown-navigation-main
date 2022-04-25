1b3cd1e0b1b6dfefd68bd1a9173107f5ae4dbac0
The-beginner-2 <vovaulchenko@gmail.com>
2022-04-25 21:21:30 +0300

    drop down menu js css

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
 "use strict";
 
+let dropDownMenuArr = document.querySelectorAll('.dropdown');
+
+dropDownMenuArr.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.target.childNodes[3].classList.toggle('sub-menu--active');
+        event.target.childNodes[1].classList.toggle('icon--active');
+    });
+});
